bef6e3449c709cd63e51297b35412dd1c625f232
Vanio-Souza <vaniooliveiratu@gmail.com>
2025-11-23 16:55:32 -0300

    att pages da uma risadinha

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,9 @@
 function Home() {
-  return <h1>Ei, obrigado por acreditar em mim, te amo.</h1>;
+  return (
+    <h1>
+      Ei, obrigado por acreditar em mim, te amo. Se você me ama da uma risadinha
+    </h1>
+  );
 }
 
 export default Home;
